050c8640880e14cf09836c2688f92f9f5c84d2bd
まちぇ <n.mache.work@gmail.com>
2024-02-09 23:18:24 +0900

    アップロード

---
+++ content.js
@@ -0,0 +1,3 @@
+var style = document.createElement("style");
+style.innerHTML = ":root{--ia4_sk_avatar-radius:50% !important;--sk_team_icon-border-radius:50% !important;} .c-base_icon--image, .c-base_icon__width_only_container, .p-r_member_profile__avatar__img{border-radius:50% !important;}";
+document.head.append(style);
